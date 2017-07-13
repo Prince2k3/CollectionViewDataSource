12f88e286016c4050e0646cd8a488c233fa82833
Prince Ugwuh <prince.ugwuh@gmail.com>
2017-07-13 17:45:38 -0500

    Added Support for Carthage

---
+++ CollectionViewDataSource/CollectionViewDataSource.h
@@ -0,0 +1,19 @@
+//
+//  CollectionViewDataSource.h
+//  CollectionViewDataSource
+//
+//  Created by Prince Ugwuh on 7/13/17.
+//  Copyright © 2017 MFX Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CollectionViewDataSource.
+FOUNDATION_EXPORT double CollectionViewDataSourceVersionNumber;
+
+//! Project version string for CollectionViewDataSource.
+FOUNDATION_EXPORT const unsigned char CollectionViewDataSourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CollectionViewDataSource/PublicHeader.h>
+
+
